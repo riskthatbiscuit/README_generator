079e661e6d1077709b6b4ef53d07f80e4dd51a6a
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-05-23 22:53:27 -0400

    Added writefile function

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Successful")
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
